45bf2ba613f4e2f5ef0dcf806c73269cf6ca1b3c
NeatRaptor <roshankalirajan4@gmail.com>
2020-12-24 16:48:28 +0530

    2 files modifies, 1 file added

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log6;
+var chain;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -14,7 +15,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -33,9 +33,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,80, PI/2);
 
     bird = new Bird(100,100);
 
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -61,4 +63,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
 }
